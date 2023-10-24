42a4478f277dabadfcd7c32eb3e237ef2df82b38
r8btx <60275558+r8btx@users.noreply.github.com>
2023-10-24 14:38:49 -0400

    SavedRoutesPage and its related files

---
+++ front-end/src/components/settings/SavedRoute.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import '../../css/settingsPage.css';
+
+const SavedRoute = ({ savedRoute }) => {
+  return (
+    <>
+      {/* Link should be targeting the actual routes page, etc. Currently just # */}
+      <Link className="settings-item routes" to={`#?from=${savedRoute.from}&to=${savedRoute.to}`}>
+        <h2 className="font-semibold">{savedRoute.name}</h2>
+        <span className="text-sm">From: {savedRoute.from}</span>
+        <span className="text-sm">To: {savedRoute.to}</span>
+      </Link>
+    </>
+  );
+};
+
+export default SavedRoute;
