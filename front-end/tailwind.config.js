80b9571f234a83404fd57bad0faf5bba0202e551
r8btx <60275558+r8btx@users.noreply.github.com>
2023-10-31 01:34:39 -0400

    added darkmode feature

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,20 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  darkMode: 'class',
   theme: {
     extend: {
       /*add more fonts here*/
-      fontFamily: { 
-        'mainUI': ['Merriweather', 'sans-serif'],
+      fontFamily: {
+        mainUI: ['Merriweather', 'sans-serif'],
       },
 
       /**change color scheme here */
       colors: {
-        darkTone : '#57078C',
+        darkTone: '#57078C',
         darkMidTone: '#998FC7',
         lightMidTone: '#C8B9F0',
         lightTone: '#F0E9FF',
-      }
+
+        // Dark mode counterparts
+        darkMode: {
+          darkTone: '#635985',
+          darkMidTone: '#443C68',
+          lightMidTone: '#393053',
+          lightTone: '#18122B',
+        },
+      },
     },
   },
   plugins: [],
