16ba7f68736f84ee3e06f5d5fd309e27bbdb475a
r8btx <60275558+r8btx@users.noreply.github.com>
2023-11-10 17:07:19 -0500

    map fix, bus data websocket, some code cleaning

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,175 +1,177 @@
-import '../css/map.css'
+import '../css/map.css';
 import { useEffect, useState, useRef } from 'react';
+import BusTrackerWebSocket from '../utils/busTrackerWebSocket';
+import { getCoordinates, generateTwoUniqueRandomInts } from '../utils/mapUtility';
 
-function Map(line) {
+function Map({ line }) {
+  const API_KEY = 'API_KEY';
+  const googleMapRef = useRef(null);
+  const [isApiLoaded, setIsApiLoaded] = useState(false);
+  const [ws, setWebSocket] = useState(null);
+  const [busData, setBusData] = useState(null);
+  const [map, setMap] = useState(null);
+  const markerRef = useRef({});
+  const [randomIntOne, randomIntTwo] = generateTwoUniqueRandomInts(0, 9);
+  const nycCoordinates = getCoordinates();
+  const [startLoc, setStartLoc] = useState(nycCoordinates[randomIntOne]);
+  const [endLoc, setEndLoc] = useState(nycCoordinates[randomIntTwo]);
 
-    const googleMapRef = useRef(null);
-    const markerRef = useRef({});
+  const mapOptions = {
+    disableDefaultUI: true, // This disables the default UI including mapTypeControl
+    zoomControl: true, // Re-enables zoom control
+  };
 
-    console.log('Map component loaded');
-    const [busData, setBusData] = useState(null);
+  // Load Google Maps API
+  useEffect(() => {
+    if (!window.google || !window.google.maps) {
+      const initMap = () => setIsApiLoaded(true);
+      window.initMap = initMap;
 
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+      script.async = true;
+      document.head.appendChild(script);
+    } else {
+      setIsApiLoaded(true); // If Google Maps API is already present
+    }
+  }, []);
 
+  // Initialize map after Google Maps API has loaded
+  useEffect(() => {
+    if (isApiLoaded) {
+      const googleMap = new window.google.maps.Map(googleMapRef.current, {
+        center: new window.google.maps.LatLng(37.7699298, -122.4469157),
+        zoom: 8,
+        options: mapOptions,
+      });
+      setMap(googleMap);
 
-    function generateTwoUniqueRandomInts(min, max) {
-        const firstInt = Math.floor(Math.random() * (max - min + 1)) + min;
-        let secondInt = Math.floor(Math.random() * (max - min + 1)) + min;
-      
-        // Ensure secondInt is different from firstInt
-        while (secondInt === firstInt) {
-          secondInt = Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-      
-        return [firstInt, secondInt];
+      window.google.maps.event.addListenerOnce(googleMap, 'tilesloaded', () => {
+        fetchBusData(googleMap);
+      });
+
+      const ws = new BusTrackerWebSocket();
+      setWebSocket(ws);
+
+      return () => {
+        ws.unsubscribe();
+      };
+    }
+  }, [isApiLoaded]);
+
+  //update map when line changes
+  useEffect(() => {
+    //wait for map to return
+    if (!startLoc || !endLoc || !map || !line || !line.lineColor) return;
+    console.log('----------------------------');
+    console.log('line changed');
+    console.log(line);
+    console.log('startLoc' + startLoc);
+    console.log('endLoc' + endLoc);
+    console.log('----------------------------');
+
+    let directionsService = new window.google.maps.DirectionsService();
+    let directionsRenderer = new window.google.maps.DirectionsRenderer({
+      polylineOptions: new window.google.maps.Polyline({
+        strokeColor: line.lineColor,
+        strokeOpacity: 0.8,
+        strokeWeight: 5,
+      }),
+    });
+    //set start and end location
+    let start = new window.google.maps.LatLng(startLoc[1], startLoc[0]);
+    let end = new window.google.maps.LatLng(endLoc[1], endLoc[0]);
+    //request settings
+    let request = {
+      origin: start,
+      destination: end,
+      travelMode: 'WALKING',
+    };
+    directionsService.route(request, function (response, status) {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(response);
+        directionsRenderer.setMap(map);
       }
-      
-    const [randomIntOne, randomIntTwo] = generateTwoUniqueRandomInts(0, 9);
+    });
+  }, [map, line, startLoc, endLoc]);
 
-    const nycCoordinates = [
-        [-73.935242, 40.730610], // Manhattan
-        [-73.944158, 40.678178], // Brooklyn
-        [-73.794851, 40.728224], // Queens
-        [-73.977622, 40.789142], // Upper West Side, Manhattan
-        [-73.939202, 40.752998], // Astoria, Queens
-        [-73.990338, 40.735781], // Greenwich Village, Manhattan
-        [-74.005941, 40.712784], // Lower Manhattan
-        [-73.949997, 40.650002], // Crown Heights, Brooklyn
-        [-73.870229, 40.773750], // Flushing, Queens
-        [-73.963548, 40.779437], // Upper East Side, Manhattan
-        // ... Add more as needed
-      ];
-    
+  useEffect(() => {
+    if (ws && busData) {
+      ws.setup(busData, setBusData);
+      ws.start();
+    }
+    processBusData(busData, map);
+  }, [busData, map]);
 
-    const [startLoc , setStartLoc] = useState(nycCoordinates[randomIntOne]);
-    const [endLoc, setEndLoc] = useState(nycCoordinates[randomIntTwo]);
-    
-    //get google from window object !!!important!!!
-    const google = window.google;
-    const [map, setMap] = useState(null);
+  const fetchBusData = async () => {
+    try {
+      console.log('fetching bus data');
+      const response = await fetch('/buses');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.log('response ok');
+      const data = await response.json();
+      console.log('bus data fetched');
+      setBusData(data);
+    } catch (error) {
+      console.log('error fetching bus data', error);
+      // Other error handling? message?
+    }
+  };
 
-    const mapOptions = {
-        disableDefaultUI: true, // This disables the default UI including mapTypeControl
-        zoomControl: true, // Re-enables zoom control
-      };
-    
-    //initialize map on component load
-    useEffect(() => {
-        const googleMap = initGoogleMap();
-        setMap(googleMap);
-        google.maps.event.addListenerOnce(googleMap, 'tilesloaded', () => {
-          fetchBusData(googleMap); // Fetch bus data after the map tiles have loaded, otherwise busses might be added before the map is initialized, which causes an error
-        });
-        // fetch bus data every 10 seconds
-        const getBusInterval = setInterval(() => {
-          fetchBusData(map);
-        }, 10000);
-        return () => {
-          clearInterval(getBusInterval); // stop fetching when map component is unmounted
-        };
-      }, []);
-
-    //update map when line changes
-    useEffect(() => {
-        //wait for map to return
-        if (!startLoc || !endLoc || !map) return;
-        console.log('----------------------------');
-        console.log('line changed');
-        console.log(line);
-        console.log('startLoc' + startLoc);
-        console.log('endLoc' + endLoc);
-        console.log('----------------------------');
-        
-        let directionsService = new google.maps.DirectionsService();
-        let directionsRenderer = new google.maps.DirectionsRenderer(
-            {
-                polylineOptions: new window.google.maps.Polyline({
-                    strokeColor: line['lineColor'],
-                    strokeOpacity: 0.8,
-                    strokeWeight: 5,
-            }
-        )});
-        //set start and end location
-        let start = new google.maps.LatLng(startLoc[1], startLoc[0]);
-        let end = new google.maps.LatLng(endLoc[1], endLoc[0]);
-        //request settings
-        let request = {
-          origin: start,
-          destination: end,
-          travelMode: 'WALKING',
-        };
-        directionsService.route(request, function (response, status) {
-          if (status === 'OK') {
-            directionsRenderer.setDirections(response);
-            directionsRenderer.setMap(map);
-          }
-        });
-
-      }, [map, line]);
-
-      const initGoogleMap = () => {
-        return new window.google.maps.Map(googleMapRef.current, {
-          center: new window.google.maps.LatLng(37.7699298, -122.4469157),
-          zoom: 8,
-          options: mapOptions,
-        });
-      };
-
-      const fetchBusData = async (Gmap) => {
-        try {
-            console.log('fetching bus data');
-            const response = await fetch('/buses');
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            else{
-              console.log('response ok');
-            }
-            console.log(response);
-            const busData = await response.json();
-            setBusData(busData);
-            console.log('bus data fetched');
-            console.log(Object.keys(busData));
-            if (busData){
-              Object.keys(busData).forEach((bus) => {
-                  //get latitute and longitude of each bus
-                  let lat = parseFloat(busData[bus][0].latitude)
-                  let lng = parseFloat(busData[bus][0].longitude)
-                  console.log('lat: ' + lat + ' lng: ' + lng)
-                  let busMarker = new google.maps.Marker({
-                    position: new google.maps.LatLng(lat, lng),
-                    map: Gmap,
-                    title: String(busData[bus][0].busId),
-                    icon: {
-                        url:'busIcon.png',
-                        scaledSize: new google.maps.Size(30, 30),
-                    }
-                  })
-                  let infowindow = new google.maps.InfoWindow({
-                    content: `<div><strong>No.${busData[bus][0].busId}</strong><br>Line: ${busData[bus][0].bus}</div>` // Example content
-                  });
-                  busMarker.addListener('click', () => {
-                    infowindow.open(Gmap, busMarker);
-                  });
-    
-                })
-              }
-              else
-              {
-                console.log('bus data not loaded');
-              }
-        } catch (error) {
-            console.log('error fetching bus data');
-            console.log(error);
+  const processBusData = (busData, map) => {
+    if (busData && map) {
+      // Remove old markers if they are no longer in the new data
+      for (const busId in markerRef.current) {
+        if (!busData.hasOwnProperty(busId)) {
+          markerRef.current[busId].setMap(null);
+          delete markerRef.current[busId];
         }
-      };
-  
-  
+      }
 
-    return (
-        <>
-            <div ref={googleMapRef} id='map'/>;
-        </>
-    )
+      Object.keys(busData).forEach((bus) => {
+        let lat = parseFloat(busData[bus][0].latitude);
+        let lng = parseFloat(busData[bus][0].longitude);
+        const position = new window.google.maps.LatLng(lat, lng);
+
+        if (markerRef.current[bus]) {
+          // Update the position of the existing marker
+          markerRef.current[bus].setPosition(position);
+        } else {
+          // Create a new marker
+          let busMarker = new window.google.maps.Marker({
+            position,
+            map,
+            title: String(busData[bus][0].busId),
+            icon: {
+              url: 'busIcon.png',
+              scaledSize: new window.google.maps.Size(30, 30),
+            },
+          });
+
+          let infowindow = new window.google.maps.InfoWindow({
+            content: `<div><strong>No.${busData[bus][0].busId}</strong><br>Line: ${busData[bus][0].bus}</div>`,
+          });
+
+          busMarker.addListener('click', () => {
+            infowindow.open(map, busMarker);
+          });
+
+          // Store the marker
+          markerRef.current[bus] = busMarker;
+        }
+      });
+    } else {
+      console.log('bus data or map not available');
+    }
+  };
+
+  return (
+    <>
+      <div ref={googleMapRef} id="map" />;
+    </>
+  );
 }
 
 export default Map;
