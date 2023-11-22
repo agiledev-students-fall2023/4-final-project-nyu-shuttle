cab8b8d56d84084913c086f03bcd0323001368ea
r8btx <60275558+r8btx@users.noreply.github.com>
2023-11-22 00:26:53 -0500

    remove backend proxy for transportation requests

--- back-end/app.js
+++ back-end/app.js
@@ -6,22 +6,4 @@ const app = express(); // instantiate an Express object
 const cors = require('cors');
 app.use(cors());
 
-const url = 'https://nyu.passiogo.com/';
-
-const buses = require('./buses.js');
-
-
-
-app.get('/buses', async (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    try {
-        console.log('buses route entered')
-        const busData = await buses.getBuses();
-        console.log('bus data fetched')
-        res.json(busData['buses']);
-      } catch (error) {
-        res.status(500).send('An error occurred while fetching the buses data');
-      }
-});
-
 module.exports = app;
