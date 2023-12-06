2ff7fb909401032784c2fdf2e4fb496bfdf965c9
r8btx <60275558+r8btx@users.noreply.github.com>
2023-12-06 00:23:58 -0500

    code style changes

--- front-end/src/utils/stops.js
+++ front-end/src/utils/stops.js
@@ -13,7 +13,6 @@ if (typeof window.nyushuttle == 'undefined') {
 window.nyushuttle.routePoints = {};
 window.nyushuttle.stops = {};
 
-
 window.nyushuttle.routes = [];
 let center = {};
 let stopMarkers = [];
@@ -74,7 +73,6 @@ export async function queryStops() {
     groupRoutes = data.groupRoutes;
     center = data.center;
     return true;
-
   } catch (error) {
     console.log('Transportations query error: ' + error.message);
     return false;
@@ -137,8 +135,8 @@ function drawRoutes(showStopName) {
     return;
   }
   let clippedRouteIndex = null;
-  if (window.nyushuttle.startStopLocation){
-    clippedRouteIndex = getClippedRoute()
+  if (window.nyushuttle.startStopLocation) {
+    clippedRouteIndex = getClippedRoute();
   }
   const routes = window.nyushuttle.routes;
 
@@ -154,24 +152,26 @@ function drawWalkingRoute(direction) {
   let request;
   let stops = window.nyushuttle.stops;
 
-  if (direction === 'from') { //plot "from" route
+  if (direction === 'from') {
+    //plot "from" route
     //becuase for some reason fromLocation.lat and fronLocation.lng are corrupted, use
     //fromLocation.place_id instead
-    startLocation = {'placeId':window.nyushuttle.fromLocation.place_id};
+    startLocation = { placeId: window.nyushuttle.fromLocation.place_id };
     endLocation = {
       lat: stops['ID' + window.nyushuttle.startStopLocation].latitude,
-      lng: stops['ID' + window.nyushuttle.startStopLocation].longitude
+      lng: stops['ID' + window.nyushuttle.startStopLocation].longitude,
     };
     request = {
       origin: startLocation,
       destination: endLocation,
       travelMode: 'WALKING',
     };
-  } else if (direction === 'to') { //plot "to" route
-    endLocation = {'placeId':window.nyushuttle.toLocation.place_id};
+  } else if (direction === 'to') {
+    //plot "to" route
+    endLocation = { placeId: window.nyushuttle.toLocation.place_id };
     startLocation = {
       lat: stops['ID' + window.nyushuttle.endStopLocation].latitude,
-      lng: stops['ID' + window.nyushuttle.endStopLocation].longitude
+      lng: stops['ID' + window.nyushuttle.endStopLocation].longitude,
     };
     request = {
       origin: startLocation,
@@ -183,72 +183,66 @@ function drawWalkingRoute(direction) {
   directionsService.route(request, function (result, status) {
     if (status == 'OK') {
       let directionsRenderer = new window.google.maps.DirectionsRenderer({
-        suppressMarkers: true // Hides the default markers
+        suppressMarkers: true, // Hides the default markers
       });
       directionsRenderer.setMap(window.nyushuttle.currentMap);
       directionsRenderer.setDirections(result);
-  
+
       // Add custom markers
-      if(direction === 'from') {
-      const startMarker = new window.google.maps.Marker({
-        position: result.routes[0].legs[0].start_location,
-        map: window.nyushuttle.currentMap,
-        icon: {
-          path: window.google.maps.SymbolPath.CIRCLE,
-          scale: 7,
-          fillColor: '#8400a8',
-          fillOpacity: 1.0,
-          strokeColor: '#FF0000',
-          strokeOpacity: 0
-        }
-      });
-    }
-    else{
-      const endMarker = new window.google.maps.Marker({
-        position: result.routes[0].legs[0].end_location,
-        map: window.nyushuttle.currentMap,
-        icon: {
-          path: window.google.maps.SymbolPath.CIRCLE,
-          scale: 7,
-          fillColor: '#8400a8',
-          fillOpacity: 1,
-          strokeColor: '#',
-          strokeOpacity: 0
-        }
-      });
-    }
+      if (direction === 'from') {
+        const startMarker = new window.google.maps.Marker({
+          position: result.routes[0].legs[0].start_location,
+          map: window.nyushuttle.currentMap,
+          icon: {
+            path: window.google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+            fillColor: '#8400a8',
+            fillOpacity: 1.0,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0,
+          },
+        });
+      } else {
+        const endMarker = new window.google.maps.Marker({
+          position: result.routes[0].legs[0].end_location,
+          map: window.nyushuttle.currentMap,
+          icon: {
+            path: window.google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+            fillColor: '#8400a8',
+            fillOpacity: 1,
+            strokeColor: '#',
+            strokeOpacity: 0,
+          },
+        });
+      }
     }
   });
 }
 
-function drawRoute(routeId, route, showStopName, clip=null) {
+function drawRoute(routeId, route, showStopName, clip = null) {
   const routeGroupId = route[2];
   const routeColor = correctColorFromARGB(route[1]);
   let routePaths = getRoutePaths(routeId);
   if (clip) {
-    drawWalkingRoute('from')
-    drawWalkingRoute('to')
-    routePaths = routePaths.slice(clip[0], clip[1])
-    if (routePaths.length == 0){
-      if (clip[0] < 100 && clip[1] > 100){
-        routePaths = routePaths.concat(routePaths.slice(100, clip[1]))
+    drawWalkingRoute('from');
+    drawWalkingRoute('to');
+    routePaths = routePaths.slice(clip[0], clip[1]);
+    if (routePaths.length == 0) {
+      if (clip[0] < 100 && clip[1] > 100) {
+        routePaths = routePaths.concat(routePaths.slice(100, clip[1]));
+      } else if (clip[0] < 100 && clip[1] < 100) {
+        routePaths = routePaths[0].slice(clip[0], clip[1]);
+      } else if (clip[0] >= 100 && clip[1] >= 100) {
+        routePaths = routePaths[1].slice(clip[0], clip[1]);
       }
-      else if (clip[0] < 100 && clip[1] < 100){
-        routePaths = routePaths[0].slice(clip[0], clip[1])
-      }
-      else if (clip[0] >= 100 && clip[1] >= 100){
-        routePaths = routePaths[1].slice(clip[0], clip[1])
-      }
-
     }
     drawRoutePath(routePaths, routeColor, routeId, routeGroupId);
-  }
-  else{
-    console.log('noclip')
+  } else {
+    console.log('noclip');
     drawRoutePath(routePaths, routeColor, routeId, routeGroupId);
-
   }
-  
+
   //   routePaths.forEach((path) => {
   //     updateBoundsWithPoint(path);
   //   });
@@ -265,45 +259,45 @@ function getRoutePaths(routeId) {
 
 function getIndexofStop(r, stopPos) {
   // Logic to get clipped route
-  let minDist = 9999
-  let index = -1
-  console.log('r: ',r)
-  for (let i=0;i<r[0].length;i++){
+  let minDist = 9999;
+  let index = -1;
+  console.log('r: ', r);
+  for (let i = 0; i < r[0].length; i++) {
     const dist = window.google.maps.geometry.spherical.computeDistanceBetween(
       new window.google.maps.LatLng(Number(r[0][i].lat), Number(r[0][i].lng)),
       new window.google.maps.LatLng(stopPos.lat, stopPos.lng)
-    )
-    if (dist < minDist){
-      minDist = dist
-      index = i
+    );
+    if (dist < minDist) {
+      minDist = dist;
+      index = i;
     }
   }
-  console.log('index: ',index)
-  return index
+  console.log('index: ', index);
+  return index;
 }
 
 function getClippedRoute() {
   let routeId = window.nyushuttle.routesSelected; // get the selected route id
-  let route = window.nyushuttle.routePoints[routeId] // get list of route points
-  let originStopId = window.nyushuttle.startStopLocation // get the origin stop id
-  let destinationStopId = window.nyushuttle.endStopLocation // get the destination stop id
-  let originStop = window.nyushuttle.stops[`ID${originStopId}`] // get the origin stop
-  let destinationStop = window.nyushuttle.stops[`ID${destinationStopId}`] // get the destination stop
-  let originStopPos = {} // get the origin stop position
-  let destinationStopPos = {} // get the destination stop position
+  let route = window.nyushuttle.routePoints[routeId]; // get list of route points
+  let originStopId = window.nyushuttle.startStopLocation; // get the origin stop id
+  let destinationStopId = window.nyushuttle.endStopLocation; // get the destination stop id
+  let originStop = window.nyushuttle.stops[`ID${originStopId}`]; // get the origin stop
+  let destinationStop = window.nyushuttle.stops[`ID${destinationStopId}`]; // get the destination stop
+  let originStopPos = {}; // get the origin stop position
+  let destinationStopPos = {}; // get the destination stop position
   //set the origin and destination stop positions
-  originStopPos.lat = originStop.latitude
-  originStopPos.lng = originStop.longitude
-  destinationStopPos.lat = destinationStop.latitude
-  destinationStopPos.lng = destinationStop.longitude
+  originStopPos.lat = originStop.latitude;
+  originStopPos.lng = originStop.longitude;
+  destinationStopPos.lat = destinationStop.latitude;
+  destinationStopPos.lng = destinationStop.longitude;
   // get the index of the origin and destination stops
-  let originStopIndex = getIndexofStop(route, originStopPos)
-  let destinationStopIndex = getIndexofStop(route, destinationStopPos)
+  let originStopIndex = getIndexofStop(route, originStopPos);
+  let destinationStopIndex = getIndexofStop(route, destinationStopPos);
   // switch places if origin stop is after destination stop
-  if (originStopIndex > destinationStopIndex){
-    return [destinationStopIndex, originStopIndex]
+  if (originStopIndex > destinationStopIndex) {
+    return [destinationStopIndex, originStopIndex];
   }
-  return [originStopIndex, destinationStopIndex]
+  return [originStopIndex, destinationStopIndex];
 }
 
 function drawRoutePath(path, routeColor, routeId, routeGroupId) {
@@ -364,8 +358,8 @@ function sliceRouteStops(routeStops, stop1, stop2) {
   // Retain the first three elements
   const initialPart = routeStops.slice(0, 3);
 
-  let startIndex = routeStops.findIndex(subarray => subarray[1] === stop1);
-  let endIndex = routeStops.findIndex(subarray => subarray[1] === stop2);
+  let startIndex = routeStops.findIndex((subarray) => subarray[1] === stop1);
+  let endIndex = routeStops.findIndex((subarray) => subarray[1] === stop2);
   if (startIndex > endIndex) {
     let temp = startIndex;
     startIndex = endIndex;
@@ -389,8 +383,12 @@ function drawStops() {
     .forEach((routeId) => {
       let routestops = routes[routeId];
       let routeGroupId = routes[routeId][2];
-      if (window.nyushuttle.startStopLocation && routestops){
-        routestops = sliceRouteStops(routestops, window.nyushuttle.startStopLocation, window.nyushuttle.endStopLocation)
+      if (window.nyushuttle.startStopLocation && routestops) {
+        routestops = sliceRouteStops(
+          routestops,
+          window.nyushuttle.startStopLocation,
+          window.nyushuttle.endStopLocation
+        );
       }
       addRouteMarkersOnMap(routeId, routestops, routeGroupId, showStopName);
     });
@@ -421,7 +419,7 @@ function addRouteMarkersOnMap(routeId, routestops, routeGroupId, showStopName) {
     if (isStopValid(theStop)) {
       updateStopData(theStop, stop, routeId, routeGroupId, routeName, routeColor);
       const marker = createMarkerForStop(theStop, zoomLevel, routeColor, showStopName, idx);
-      marker.addListener('click', () => onMarkerClick(theStop, marker)); 
+      marker.addListener('click', () => onMarkerClick(theStop, marker));
       stopMarkers.push(marker);
     }
   });
@@ -432,92 +430,99 @@ function getCorrespondingRoute(routeIDs) {
   if (typeof routeIDs === 'string') {
     routeIDs = [routeIDs];
   }
-  for(let i = 0; i < routeIDs.length; i++) {
+  for (let i = 0; i < routeIDs.length; i++) {
     let route = routeIDs[i];
-    if (route === "44748") {
-      routes.push("C");
-    } else if(route === "44676") {
-      routes.push("A");
-    } else if(route === "44753") {
-      routes.push("W");
-    } else if(route === "44745") {
-      routes.push("B");
-    } else if(route === "41890") {
-      routes.push("CH");
-    } else if(route === "44749") {
-      routes.push("E");
-    } else if(route === "44750") {
-      routes.push("F");
-    } else if(route === "44752") {
-      routes.push("G");
-    } else if(route === "44754") {
-      routes.push("MP");
-    } else if(route === "44755") {
-      routes.push("ME");
-    } else if(route === "44756") {
-      routes.push("MW");
-    } else if(route === "44757") {
-      routes.push("BL");
-    } else if(route === "45769") {
-      routes.push("FR");
+    if (route === '44748') {
+      routes.push('C');
+    } else if (route === '44676') {
+      routes.push('A');
+    } else if (route === '44753') {
+      routes.push('W');
+    } else if (route === '44745') {
+      routes.push('B');
+    } else if (route === '41890') {
+      routes.push('CH');
+    } else if (route === '44749') {
+      routes.push('E');
+    } else if (route === '44750') {
+      routes.push('F');
+    } else if (route === '44752') {
+      routes.push('G');
+    } else if (route === '44754') {
+      routes.push('MP');
+    } else if (route === '44755') {
+      routes.push('ME');
+    } else if (route === '44756') {
+      routes.push('MW');
+    } else if (route === '44757') {
+      routes.push('BL');
+    } else if (route === '45769') {
+      routes.push('FR');
     }
   }
   return routes;
 }
 
 async function onMarkerClick(theStop, marker) {
-  const stopName = marker.title; 
-  console.log(stopName)
-  console.log(theStop.routeIDs)
+  const stopName = marker.title;
+  console.log(stopName);
+  console.log(theStop.routeIDs);
   const routes = getCorrespondingRoute(theStop.routeIDs);
-  console.log(routes)
+  console.log(routes);
   let next_times = {};
-  for(let i = 0; i < routes.length; i++) {
-    if(checkIfInfoisAvailable(routes[i])){
+  for (let i = 0; i < routes.length; i++) {
+    if (checkIfInfoisAvailable(routes[i])) {
       let route = routes[i];
       const adjustedStopName = await getMatchingName(stopName, route);
       const times = await getNextTimes(encodeURIComponent(adjustedStopName), route);
       next_times[route] = times;
-    } else{
-      next_times[0] = ["No info available"];
+    } else {
+      next_times[0] = ['No info available'];
     }
   }
   console.log(next_times);
   displayStopInfo(marker, stopName, next_times);
 }
 
-async function displayStopInfo(marker, stopName, next_times){
+async function displayStopInfo(marker, stopName, next_times) {
   const contentString = buildInfoContent(stopName, next_times);
   const infowindow = new window.google.maps.InfoWindow({
     content: contentString,
-    ariaLabel: "Uluru",
+    ariaLabel: 'Uluru',
   });
   infowindow.open(window.google.maps, marker);
 }
 
 function buildInfoContent(stopName, next_times) {
-  let content = "<div>";
+  let content = '<div>';
   content += `<p>${stopName}</p>`;
   for (const route in next_times) {
     if (next_times.hasOwnProperty(route)) {
       const times = next_times[route];
-      if (times != null && times[0] !== "No info available" && times.length !== 0) {
+      if (times != null && times[0] !== 'No info available' && times.length !== 0) {
         content += `<p>Route ${route}: ${times.join(', ')}</p>`;
-      } else if(times[0] === "No info available") {
+      } else if (times[0] === 'No info available') {
         content += `<p>No times available for this route. Please check passiogo for available times.</p>`;
-      }else {
+      } else {
         content += `<p>Route ${route}: No incoming shuttles at this stop.</p>`;
       }
     }
   }
-  content += "</div>";
+  content += '</div>';
   return content;
 }
 
 function checkIfInfoisAvailable(route_id) {
-  if(route_id === "MP" || route_id === "ME" || route_id === "MW" || route_id === "BL" || route_id === "FR" || route_id === "CH"){
-    return false; 
-  } 
+  if (
+    route_id === 'MP' ||
+    route_id === 'ME' ||
+    route_id === 'MW' ||
+    route_id === 'BL' ||
+    route_id === 'FR' ||
+    route_id === 'CH'
+  ) {
+    return false;
+  }
   return true;
 }
 
@@ -606,11 +611,11 @@ function recreateStopMarkerCluster() {
 
 async function onClusterMarkerClick(cluster) {
   const markers = cluster.getMarkers();
-  console.log(markers)
-  if (markers){
+  console.log(markers);
+  if (markers) {
     //onMarkerClick(theStop, markers[0])
   }
-  
+
   //onClusterMarkerClick(markers[0]);
   //console.log(markers);
 }
